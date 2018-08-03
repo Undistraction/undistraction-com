58753f110786486e621bde5e04f228387569d7d4
Pedr Browne <pedr.browne@gmail.com>
2018-08-03 10:44:33 +0100

    debug: Remove all plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,40 +1,13 @@
 const validatedConfig = require(`./src/config/validatedConfig`)
-const feed = require(`./src/config/plugins/feed`)
-const sitemap = require(`./src/config/plugins/sitemap`)
-const robots = require(`./src/config/plugins/robots`)
-const favicon = require(`./src/config/plugins/favicon`)
 const sourceFileSystem = require(`./src/config/plugins/sourceFilesystem`)
-const googleAnalytics = require(`./src/config/plugins/googleAnalytics`)
-const remark = require(`./src/config/plugins/remark`)
 
 const config = validatedConfig()
-const { seo } = config
 
 // -----------------------------------------------------------------------------
 // Define Plugins Used
 // -----------------------------------------------------------------------------
 
-const plugins = [
-  // SSR support for styled components
-  `gatsby-plugin-sharp`,
-  // Add support for image nodes using Sharp
-  `gatsby-transformer-sharp`,
-  // Support local links in markdown using pushstate
-  `gatsby-plugin-catch-links`,
-  // Remove trailing slashes from urls
-  `gatsby-plugin-remove-trailing-slashes`,
-  // Audit page for accessibility issues
-  // Toggle to check for accessiblity issues
-  favicon(),
-  sourceFileSystem(`/pages`),
-  remark(config),
-]
-
-// Add support for Google analytics if a tracking code was defined in
-// site-config.js
-if (seo.googleTrackingID) {
-  plugins.push(googleAnalytics(seo))
-}
+const plugins = [sourceFileSystem(`/pages`)]
 
 module.exports = {
   // pathPrefix: '/example',
