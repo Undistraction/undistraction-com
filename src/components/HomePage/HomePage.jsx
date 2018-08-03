4843417c929c4dea7eb133a9402cc51262b62980
Pedr Browne <pedr.browne@gmail.com>
2018-08-03 10:18:08 +0100

    debug: Remove UI

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,17 +1,5 @@
-import PropTypes from 'prop-types'
 import React from 'react'
-import { markdownItemImageSizes } from '../helpers/markdown'
-import { homePageTitle } from '../helpers/siteMetadata'
-import Page from '../shared/Page'
 
-const HomePage = ({ data }) => (
-  <Page title={homePageTitle(data)} imageSizes={markdownItemImageSizes(data)}>
-    <h1>Hello</h1>
-  </Page>
-)
-
-HomePage.propTypes = {
-  data: PropTypes.object.isRequired,
-}
+const HomePage = () => <h1>Hello</h1>
 
 export default HomePage
