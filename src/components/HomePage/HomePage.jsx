140221ac2409cf30b71a7b7763869efe48e423ae
Pedr Browne <pedr.browne@gmail.com>
2018-08-03 10:16:13 +0100

    debug: Strip away UI

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,41 +1,12 @@
-import { api } from 'cssapi'
 import PropTypes from 'prop-types'
 import React from 'react'
-import styled from 'styled-components'
-import MarkdownContent from '../../components/shared/MarkdownContent'
-import {
-  markdownItemHTMLAst,
-  markdownItemImageSizes,
-} from '../helpers/markdown'
-import { homePageMetadata, homePageTitle } from '../helpers/siteMetadata'
-import Metadata from '../shared/Metadata'
+import { markdownItemImageSizes } from '../helpers/markdown'
+import { homePageTitle } from '../helpers/siteMetadata'
 import Page from '../shared/Page'
-import Front from './Front/Front'
-
-const TextContent = styled.div`
-  margin: 0 auto;
-  ${api({
-    maxWidth: 900,
-    paddingH: {
-      default: `1ru`,
-      'mediumUp+100': 0,
-    },
-  })};
-`
-
-const Layout = styled.div`
-  ${api({})};
-`
 
 const HomePage = ({ data }) => (
   <Page title={homePageTitle(data)} imageSizes={markdownItemImageSizes(data)}>
-    <Layout>
-      <Metadata {...homePageMetadata(data)} />
-      <Front />
-      <TextContent>
-        <MarkdownContent htmlAst={markdownItemHTMLAst(data)} />
-      </TextContent>
-    </Layout>
+    <h1>Hello</h1>
   </Page>
 )
 
