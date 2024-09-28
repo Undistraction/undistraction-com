5bbfe179c386615d33760aea5a04f4a025d4b37e
Pedr <pedr.browne@gmail.com>
2024-09-28 13:44:15 +0100

    feat: Left-align resume link

--- src/components/ResumeLink/ResumeLink.jsx
+++ src/components/ResumeLink/ResumeLink.jsx
@@ -5,7 +5,7 @@
 export default function ResumeLink() {
   return (
     <a
-      className="relative mx-auto block max-w-72 p-3 text-center font-bold after:absolute after:inset-0 after:rounded-lg after:border after:border-gray-100 after:content-[''] hover:after:border-2 hover:after:border-gray-400"
+      className="relative block max-w-72 p-3 text-center font-bold after:absolute after:inset-0 after:rounded-lg after:border after:border-gray-100 after:content-[''] hover:after:border-2 hover:after:border-gray-400"
       href="/pdf/Pedr-Browne-résumé.v2.pdf"
     >
       Pedr Browne Rėsumė
