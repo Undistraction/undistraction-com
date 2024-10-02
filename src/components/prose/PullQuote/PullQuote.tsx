248552c7f3bbd2521118e08dcd0c7df3eab92c46
Pedr <pedr.browne@gmail.com>
2024-10-02 12:02:28 +0100

    feat: Layout improvements

--- src/components/prose/PullQuote/PullQuote.tsx
+++ src/components/prose/PullQuote/PullQuote.tsx
@@ -14,10 +14,18 @@ interface PullQuoteProps {
 export default function PullQuote({ quote, citation }: PullQuoteProps) {
   return (
     <blockquote
-      className="relative max-w-[420px] space-y-3 pl-6 text-lg/7 after:absolute after:bottom-2 after:left-0 after:top-1 after:border-l-2 after:border-gray-400 after:content-[''] md:pl-0 md:after:-left-6"
+      className="relative max-w-[420px] space-y-1 text-lg/7"
       aria-label="Pull quote"
     >
-      <span className="italic">{quote}</span>
+      <span className="relative italic">
+        <span className="absolute -left-[10px] text-gray-400 md:-left-[35px] md:text-6xl">
+          “
+        </span>
+        {quote}
+        <span className="absolute right-[-10px] text-gray-400 md:-bottom-[32px] md:-right-[22px] md:text-6xl">
+          ”
+        </span>
+      </span>
       <cite
         className="font-quote block text-base not-italic text-gray-400"
         aria-label="Citation"
--- src/components/site/SiteHeader/SiteHeader.tsx
+++ src/components/site/SiteHeader/SiteHeader.tsx
@@ -7,7 +7,7 @@ import SiteBrandingLink from './SiteBrandingLink'
 
 export default function SiteHeader() {
   return (
-    <header className="flex flex-row justify-between py-4 align-middle md:py-6">
+    <header className="relative flex flex-row justify-between py-4 align-middle after:absolute after:inset-x-0 after:bottom-0 after:h-[1px] after:bg-gradient-to-r after:from-gray-800 after:via-gray-600 after:to-gray-800 after:content-[''] md:py-6">
       <SiteBrandingLink />
       <SiteNav />
     </header>
